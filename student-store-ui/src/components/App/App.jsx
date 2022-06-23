f8a396ba00f6042d75825e46f652a5f5874613a7
Eva Sennrich <evasennrich@gmail.com>
2022-06-23 16:00:46 -0700

    products returned by response in state variable

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -6,8 +6,49 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProductDetail from "../ProductDetail/ProductDetail";
 import NotFound from "../NotFound/NotFound";
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
 
 export default function App() {
+  const [products, setProducts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState("error");
+  //   shoppingCart - should store state for the active user's shopping cart (items they want to purchase and the quantity of each item).
+  //  Use whatever data type works best here, but make sure the format the shoppingCart as an array before passing it to other components.
+  //  When passed down to other components as a prop, it should formatted as an array of objects.
+  //  Each object in the array should have two fields:
+  //  The itemId field should store the id of the item being purchased.
+  //  The quantity field should store a number representing how many of that item the user is purchasing.
+  //  checkoutForm - the user's information that will be sent to the API when they checkout.
+  const [isOpen, setIsOpen] = useState(false);
+  const [shoppingCart, setShoppingCart] = useState([]);
+
+  //Fetching data from API func
+  //  It should make a GET request to the API's /store endpoint with the axios.get method.
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("https://codepath-store-api.herokuapp.com/store");
+      // console.log(response.data);
+      setProducts(response.data);
+      console.log(products);
+    } catch {
+      //  If the request does not complete successfully, or there are no products found in the response, it should create an error message and store it in the error state variable.
+      // setError("no products found");
+      console.log(error);
+    }
+  };
+
+  //useEffect calling the fethData func in first reload!
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleOnToggle = () => {
+    //  It should toggle the open/closed state of the Sidebar.
+  };
+  const handleAddItemToCart = () => {};
+
   return (
     <div className="app">
       {/*  this helps to render/ call every single component */}
---
+++ student-store-ui/src/components/CheckoutForm/CheckoutForm.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const CheckoutForm = () => {
+  return <div>CheckoutForm</div>;
+};
+
+export default CheckoutForm;
+
+//check the slides from today 6/22 week 2 day 3!!
