209713700c9f3b982575f18376a5d84d18527360
Eva Sennrich <evasennrich@gmail.com>
2022-06-30 20:16:57 -0700

    routes and models  set

--- student-store-express-api/model/store.js
+++ student-store-express-api/model/store.js
@@ -25,39 +25,4 @@ class Store {
   }
 }
 
-// static async recordTransaction(transaction) {
-//     // create a new transaction
-
-//     if (!transaction) {
-//       throw new BadRequestError(`No transaction sent.`)
-//     }
-//     const requiredFields = ["description", "category", "amount"]
-//     requiredFields.forEach((field) => {
-//       if (!transaction[field] && transaction[field] !== 0) {
-//         throw new BadRequestError(`Field: "${field}" is required in transaction`)
-//       }
-//     })
-
-//     const transactions = await Bank.listTransactions()
-//     const transactionId = transactions.length + 1
-//     const postedAt = new Date().toISOString()
-
-//     const newTransaction = { id: transactionId, postedAt, ...transaction }
-
-//     storage.get("transactions").push(newTransaction).write()
-
-//     return newTransaction
-//   }
-
-//============================================
-// Models - The API should use a Store model that handles the following
-//  List all products currently in the db.json file
-//  Fetch a single product by its id
-//  Create a purchase order
-//==================================================
-
-//this code list all products currently in the db.json file
-// Storage.get("products").value();
-//get products and return one single product
-
 module.exports = Store;
--- student-store-express-api/routes/store.js
+++ student-store-express-api/routes/store.js
@@ -16,23 +16,17 @@ router.get("/", (req, res, next) => {
 router.get("/:productId", (req, res, next) => {
   //this var stores the code for getting the param "productId" in order to get a single product by its ID. Example: http://localhost:3001/store/3
   const productID = req.params.productId;
-  console.log(req.params);
   const product = store.productsByID(productID);
-  //this line is sending a response with a status of "200" code and a JSON response with an key of product and value of : product (which is the)
   res.status(200).json({ product: product });
 });
 
 //  It should allow POST requests to the /store endpoint:
 //  The endpoint should create purchase orders for users and save them to the db.json file
 // // create a new transfer
-// router.post("/", async (req, res, next) => {
-//   try {
+// router.post("/store", (req, res, next) => {
 //     const transfer = req.body.transfer;
 //     const newTransfer = await Bank.recordTransfer(transfer);
 //     res.status(201).json({ transfer: newTransfer });
-//   } catch (err) {
-//     next(err);
-//   }
 // });
 
 module.exports = router;
